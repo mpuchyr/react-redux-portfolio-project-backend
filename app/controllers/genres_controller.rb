dd2db44bff2fa0444241b129be9279eef6f00c9e
mpuchyr <matt.puchyr@gmail.com>
2020-02-10 14:52:08 -0600

    Add index and show actions to movie and genre controllers

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,11 @@
 class GenresController < ApplicationController
+    def index
+        genres = Genre.all
+        render json: genres
+    end
+
+    def show
+        genre = Genre.find_by(id: params[:id])
+        render json: genres
+    end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,12 @@
 class MoviesController < ApplicationController
+
+    def index 
+        movies = Movie.all
+        render json: movies
+    end
+
+    def show
+        movie = Movie.find_by(id: params[:id])
+        render json: movie, include: [:genres]
+    end
 end
