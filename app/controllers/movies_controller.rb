a6efb90af6dfb10e45eefea633ea0f15ef4b68a9
mpuchyr <matt.puchyr@gmail.com>
2020-02-11 19:37:58 -0600

    Add create action to movies_controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,4 +9,12 @@ class MoviesController < ApplicationController
         movie = Movie.find_by(id: params[:id])
         render json: movie, include: [:genre]
     end
+
+    def create
+        movie = Movie.find_by(title: params[:title])
+        if !movie
+            movie = Movie.create(title: params[:title], synopsis: params[:synopsis], poster_url: params[:poster_url])
+        end
+        redirect_to movies_path
+    end
 end
