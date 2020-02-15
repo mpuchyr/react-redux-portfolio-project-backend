90872bee04ef44ec87fd33869c46fda1f024bd96
mpuchyr <matt.puchyr@gmail.com>
2020-02-14 19:02:53 -0600

    Adds a generic poster_url if one wasn't provided in params in movies_controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,8 +13,13 @@ class MoviesController < ApplicationController
     def create
         movie = Movie.find_by(title: params[:title])
         if !movie
+            if params[:poster_url] == ""
+                poster = "https://i.pinimg.com/originals/9e/4b/97/9e4b97433364d774a2a4a9c6290e8906.jpg"
+            else 
+                poster = params[:poster_url]
+            end
             genre = Genre.find_or_create_by(name: params[:genre])
-            movie = Movie.create(title: params[:title], synopsis: params[:synopsis], poster_url: params[:poster_url], genre_id: genre.id)
+            movie = Movie.create(title: params[:title], synopsis: params[:synopsis], poster_url: poster, genre_id: genre.id)
         end
         redirect_to movies_path
     end
